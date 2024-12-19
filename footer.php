b029139cfded36a3221f63c686ceafd8b6a8c366
Michał Szumielewicz <admin@Mac.lan>
2024-12-19 12:21:06 +0100

    Add new footer

--- footer.php
+++ footer.php
@@ -5,67 +5,43 @@ $linkedin = get_field('linkedin_link', 'option') ? get_field('linkedin_link', 'o
 $youtube = get_field('youtube_link', 'option') ? get_field('youtube_link', 'option') : null;
 $email_address = get_field('email_address', 'option') ? get_field('email_address', 'option') : '';
 $copyright_text = get_field('copyright_text', 'option') ? get_field('copyright_text', 'option') : '';
+$footer_logo = get_field('footer_logo', 'option') ? get_field('footer_logo', 'option') : '';
+$footer_title = get_field('footer_title', 'option') ? get_field('footer_title', 'option') : '';
+$footer_description = get_field('footer_description', 'option') ? get_field('footer_description', 'option') : '';
 ?>
 </div><!-- #CONTENT END -->
 </main>
 
 <footer class="footer fullwidth flex vertical vertical__center">
     <div class="row boxwidth">
-        <div class="col col-xxs-12 col-xs-12 col-sm-4 footer__column">
-            <object class="footer__logo" data="<?= IMAGES ?>" type="image/svg+xml">
-                LOGO
-            </object>
+        <div class="col col-xxs-12 col-xs-12 col-sm-6 footer__column">
+            <p class="footer__title"><?= $footer_title ?></p>
+            <p class="footer__description"><?= $footer_description ?></p>
+        </div>
+        <div class="col col-xxs-12 col-xs-12 col-sm-6 footer__logo vertical__center">
+            <object data="<?= $footer_logo['url'] ?>" type="image/svg+xml"></object>
             <div class="footer__social">
                 <?php if ($facebook) : ?>
-                    <a href="<?= $facebook ?>" target="_blank"><svg>
-                            <use href="#facebook" />
-                        </svg></a>
+                <a href="<?= $facebook ?>" target="_blank"><svg>
+                        <use href="#facebook" />
+                    </svg></a>
                 <?php endif;
                 if ($instagram) : ?>
-                    <a href="<?= $instagram ?>" target="_blank"><svg>
-                            <use href="#instagram" />
-                        </svg></a>
+                <a href="<?= $instagram ?>" target="_blank"><svg>
+                        <use href="#instagram" />
+                    </svg></a>
                 <?php endif;
                 if ($linkedin) : ?>
-                    <a href="<?= $linkedin ?>" target="_blank"><svg>
-                            <use href="#linkedin" />
-                        </svg></a>
+                <a href="<?= $linkedin ?>" target="_blank"><svg>
+                        <use href="#linkedin" />
+                    </svg></a>
                 <?php endif; ?>
                 <?php if ($youtube) : ?>
-                    <a href="<?= $youtube ?>" target="_blank"><svg>
-                            <use href="#youtube" />
-                        </svg></a>
+                <a href="<?= $youtube ?>" target="_blank"><svg>
+                        <use href="#youtube" />
+                    </svg></a>
                 <?php endif; ?>
             </div>
-        </div>
-        <div class="col col-xxs-12 col-xs-12 col-sm-4 footer__column">
-
-            <?php
-            /*
-                *
-                * Widget - footer column 2
-                * Remove it for adding custom elements
-                *
-                */
-            if (is_active_sidebar("footer-2")) :
-            ?>
-
-                <div class=""><?php dynamic_sidebar("footer-2"); ?></div>
-            <?php endif ?>
-        </div>
-        <div class="col col-xxs-12 col-xs-12 col-sm-4 footer__column">
-            <?php
-            /*
-                *
-                * Widget - footer column 2
-                * Remove it for adding custom elements
-                *
-                */
-            if (is_active_sidebar("footer-3")) :
-            ?>
-
-                <div class=""><?php dynamic_sidebar("footer-3"); ?></div>
-            <?php endif ?>
         </div>
         <div class="col col-xxs-12 col-xs-12 footer__copyrights">
             <?= $copyright_text ?>
