99b8d74997dd54be059ed50ecf53769cbe15e6ec
Pawel Baczynski <pawel.baczynski@bravenew.agency>
2024-12-21 12:35:44 +0100

    Mobile improvements

--- src/js/modules/nav-fixed.js
+++ src/js/modules/nav-fixed.js
@@ -8,7 +8,7 @@ export const navFixed = () => {
 
     const nav = document.querySelector(".header");
 
-    if (window.pageYOffset > 600) {
+    if (window.pageYOffset > 700) {
         nav.classList.add("--fixed");
     } else {
         nav.classList.remove("--fixed");
