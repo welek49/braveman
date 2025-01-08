27f28bc9442a5efc28f48411f3f1673cd809e1a6
Michał Szumielewicz <admin@Mac.lan>
2025-01-08 23:48:29 +0100

    fix read more

--- src/js/modules/read-more.js
+++ src/js/modules/read-more.js
@@ -5,38 +5,38 @@
  */
 
 export const readMore = () => {
-    const readMoreElements = document.querySelectorAll(".read-more__content");
+    const readMoreElements = document.querySelectorAll(".read-more");
 
     if (readMoreElements.length > 0) {
-        readMoreElements.forEach(function (element) {
-            const initialHeight = element.classList.contains(
-                "read-more__content"
-            )
-                ? `${heightReadMore}px`
-                : "20px";
-            element.style.maxHeight = initialHeight;
-            element.style.transition = "max-height 0.5s ease";
+        readMoreElements.forEach(function (readMoreElement) {
+            const contentElement = readMoreElement.querySelector(
+                ".read-more__content"
+            );
+            const initialHeight = `${heightReadMore}px`;
+            contentElement.style.maxHeight = initialHeight;
+            contentElement.style.transition = "max-height 0.5s ease";
 
             const label = document.createElement("span");
             label.classList.add("read-more__label");
             label.textContent = labelReadMore;
-            element.appendChild(label);
+            readMoreElement.appendChild(label);
 
             function toggleElement() {
-                const isCollapsed = element.style.maxHeight === initialHeight;
+                const isCollapsed =
+                    contentElement.style.maxHeight === initialHeight;
 
                 if (isCollapsed) {
-                    element.style.maxHeight = element.scrollHeight + "px";
-                    label.style.display = "none";
+                    contentElement.style.maxHeight =
+                        contentElement.scrollHeight + "px";
                     label.textContent = labelReadLess;
+                    label.classList.add("expanded");
                 } else {
-                    element.style.maxHeight = initialHeight;
-                    label.style.display = "block";
+                    contentElement.style.maxHeight = initialHeight;
                     label.textContent = labelReadMore;
+                    label.classList.remove("expanded");
                 }
             }
 
-            element.addEventListener("click", toggleElement);
             label.addEventListener("click", function (event) {
                 event.stopPropagation();
                 toggleElement();
